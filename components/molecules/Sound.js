fe9057a160e07090b8f83262aa4b9ac4fbbe4b22
thomasdriscoll <thomasdriscoll98@gmail.com>
2020-03-07 18:24:49 -0600

    working sound on Android mobile

--- components/molecules/Sound.js
+++ components/molecules/Sound.js
@@ -4,31 +4,50 @@ import { Audio } from 'expo-av';
 
 let playedFlag = false;
 
-export function Sound({ navigation }){
-    return(
-        <View style={styles.container}>
-            {/* Insert sound image */}
-            <Button 
-                title="Play"
-                onPress= {() => {
-                    playSound();
-                    playedFlag = true;
-                }}
-                style={styles.button}
-                // Insert "play" image here
-            /> 
-            <Button 
-                title="Next"
-                onPress = {() => {
-                    if(playedFlag){
-                        playedFlag = false;
-                        navigation.navigate('Choice')
-                    }
-                }}
-                style = {styles.button}
-            />           
-        </View>
-    );
+export class Sound extends React.Component{
+    constructor(props){
+        super(props);
+        this.navigation = this.props.navigation;
+    }
+
+    componentDidMount(){
+        Audio.setAudioModeAsync({
+            allowsRecordingIOS: false,
+            staysActiveInBackground: false,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+            shouldDuckAndroid: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+            playThroughEarpieceAndroid: false
+        });
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                {/* Insert sound image */}
+                <Button 
+                    title="Play"
+                    onPress= {() => {
+                        playSound();
+                        playedFlag = true;
+                    }}
+                    style={styles.button}
+                    // Insert "play" image here
+                /> 
+                <Button 
+                    title="Next"
+                    onPress = {() => {
+                        if(playedFlag){
+                            playedFlag = false;
+                            this.navigation.navigate('Choice')
+                        }
+                    }}
+                    style = {styles.button}
+                />           
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -45,10 +64,41 @@ const styles = StyleSheet.create({
 async function playSound(){
     const soundObject = new Audio.Sound();
     try{
-        await soundObject.loadAsync(require('../../assets/sounds/DL_'+(global.position+1).toString()+'.mp4'));
+        switch(global.position){
+            case 0:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_1.mp4'));
+                break;
+            case 1:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_2.mp4'));
+                break;
+            
+            case 2:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_3.mp4'));
+                break;
+            case 3:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_4.mp4'));
+                break;
+            case 4:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_5.mp4'));
+                break;
+            case 5:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_6.mp4'));
+                break;
+            case 6:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_7.mp4'));
+                break;
+            case 7:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_8.mp4'));
+                break;
+            case 8:
+                await soundObject.loadAsync(require('../../assets/sounds/DL_9.mp4'));
+                break;
+            default:
+                break;
+        }
         await soundObject.playAsync();
     }
     catch(err){
-        console.log('Never works on the first try :/');
+        console.log(err);
     }
 }
